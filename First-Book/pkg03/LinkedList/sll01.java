108a1b943ea66c94b97ff328b7aa114600f47d53
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-12-27 06:52:52 +0000

    update

--- First-Book/pkg03/LinkedList/sll01.java
+++ First-Book/pkg03/LinkedList/sll01.java
@@ -14,8 +14,60 @@ public class sll01 <E> extends Node {
     }
     private int getSize(){return size;}
 
-    public boolean isEmpty(){return size==0? true:false;}
+    public boolean isEmpty(){return size==0? true:false;} // use ternary betters for visual, just size==0 would be same
 
     //returns the first elem
-    public E first(){return head.getElement();}
+    public E first(){
+        if(isEmpty()){
+            return null;
+        } else {
+        return head.getElement();
+        }
+    }
+        public E last(){
+        if(isEmpty()){
+            return null;
+        } else {
+        return tail.getElement();
+        }
+    }
+
+    // setters method
+
+    public void addFirst(E e){
+        // creating the "new head" by linking the old head to the new element
+        head = new Node<>(e,head);
+        if(size==0){
+            tail = head;
+        }
+        size++;
+    }
+
+    public void addLast( E e ){
+        // ADDING. at the tail of the list, since it is tail it would point to null
+        Node<E> newest = new Node<>(e,null);
+        if(isEmpty()){
+            head = newest; 
+        } else 
+        {
+            tail.setNext(newest);
+            tail = newest;
+            size++;
+        }
+    }
+    // remove the first and return 
+    public E removeFirst(){
+        if(isEmpty()){
+            return null;
+        }
+            E answer = head.getElement();
+            head = head.getNext();
+            size--;
+ 
+
+            return answer;
+        }
+
+
 }
+
