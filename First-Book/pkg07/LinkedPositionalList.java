a93bc19f8074762fccfb9184e85a0ca26b7678c1
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-16 21:25:48 +0000

    update

--- First-Book/pkg07/LinkedPositionalList.java
+++ First-Book/pkg07/LinkedPositionalList.java
@@ -3,6 +3,40 @@ public class LinkedPositionalList<E> implements PositionalList<E> {
         private E element;
         private Node<E> prev;
         private Node<E> next;
-    }
+        public Node(E e, Node<E> p, Node<E> n){
+            element = e ;
+            prev = p;
+            next = n;
+        }
+        public E getElement() throws IllegalStateException{
+            if(next==null)
+                throw new IllegalStateException("Position no longer valid");
+            return element;
+        }
+        public Node<E> getPrev(){
+            return prev;
+        }
+        public Node<E> getNext(){
+            return next;
+        }
+        public void setElement(E e){
+            element = e;
+        }
+        public void setPrev(Node<E> p){
+            prev = p;
+        }
+        public void setNext(Node<E> n){
+            next = n;
+        }
+    } // ------------end of nested Node class-----------
+
+    private Node<E> header;
+    private Node<E> trailer;
+    private int size = 0;
     
+    public LinkedPositionalList(){
+        header = new Node<>(null,null,null);
+        trailer = new Node<>(null,header,null);
+        header.setNext(trailer);
+    }
 }
