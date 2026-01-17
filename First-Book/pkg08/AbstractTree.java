67540023fdc44579d5749746a38aaa58b8e47724
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-17 23:34:14 +0000

    update

--- First-Book/pkg08/AbstractTree.java
+++ First-Book/pkg08/AbstractTree.java
@@ -9,5 +9,13 @@ public class AbstractTree<E> implements Tree<E> {
         else
             return 1+depth(parent(p));
     }
-    
+
+    public int height(Position<E> p){
+        int h = 0;
+        for(Position<E> c : children(p))
+            h = Math.max(h, 1+height(c));
+        return h;
+    }
+
+
 }
