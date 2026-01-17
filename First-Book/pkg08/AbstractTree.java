a642fde47b970465f83df6477dac0dc6dd32010d
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-01-17 23:32:56 +0000

    updat

--- First-Book/pkg08/AbstractTree.java
+++ First-Book/pkg08/AbstractTree.java
@@ -3,5 +3,11 @@ public class AbstractTree<E> implements Tree<E> {
     public boolean isExternal(Position<E> p) {return numChildren(p) == 0;}
     public boolean isRoot(Position<E> p) {return p==root();}
     public boolean isEmpty(){return size() == 0;}
+    public int depth(Position<E> p){
+        if(isRoot(p))
+            return 0;
+        else
+            return 1+depth(parent(p));
+    }
     
 }
