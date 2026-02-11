a060198acb45406d84f56bc0b89426687fd4b81f
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-02-11 17:15:19 +0000

    update

--- Practice/LinkedList/SimpleLinkedList.java
+++ Practice/LinkedList/SimpleLinkedList.java
@@ -1,6 +1,24 @@
 package LinkedList;
 
 public class SimpleLinkedList<E> {
+
+    private static class SimpleNode <E> {
+        SimpleNode<E> next;
+        E data;
+
+        /** 
+         * construct new node with the data and new reference
+         * @param E data, SimpleNode<E> next
+         * @param data the data element to be stored in the node.
+         * @param next the reference to the next node in the list.
+         */
+        public SimpleNode(E data, SimpleNode<E> next){
+            this.next = next;
+            this.data = data;
+        }
+    }
+
+
     private SimpleNode<E> head;
     private SimpleNode<E> tail;
     private int totalSize;
@@ -15,5 +33,7 @@ public class SimpleLinkedList<E> {
         totalSize = 0;
 
     }
-    
+    public void add(E data){
+
+    }
 }
