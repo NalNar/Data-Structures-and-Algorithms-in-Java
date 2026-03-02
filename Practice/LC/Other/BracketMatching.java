b0efae82d441df853907c8aa772f5bd772c25327
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-03-02 21:32:04 +0000

    update

--- Practice/LC/Other/BracketMatching.java
+++ Practice/LC/Other/BracketMatching.java
@@ -1,5 +1,29 @@
 package Other;
+import java.util.*;
 
 public class BracketMatching {
+    public static boolean bracketMatching(String input){
+        Stack<Character> s = new Stack<>();
+        
+        for(int i =0; i<input.length(); i++){
+            if(input.charAt(i) == '('){
+                s.push('(');
+            }
+            if(input.charAt(i) == ')'){
+                if (s.isEmpty()){
+                    return false;
+                }
+                s.pop();
+            }
+            
+        }
+
+        return s.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "4x((2-1()";
+        System.out.println(bracketMatching(s));
+    }
     
 }
