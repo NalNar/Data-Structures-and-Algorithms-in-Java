dd36f2e5d39bb4f1a59da08111fc618ec4658fed
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-03-02 21:48:30 +0000

    update

--- Practice/LC/Other/BracketMatching.java
+++ Practice/LC/Other/BracketMatching.java
@@ -2,28 +2,48 @@ package Other;
 import java.util.*;
 
 public class BracketMatching {
+
     public static boolean bracketMatching(String input){
+
         Stack<Character> s = new Stack<>();
-        
-        for(int i =0; i<input.length(); i++){
-            if(input.charAt(i) == '('){
-                s.push('(');
-            }
-            if(input.charAt(i) == ')'){
-                if (s.isEmpty()){
+
+        for(int i = 0; i<input.length(); i++){
+            char ch = input.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                    s.push(ch);
+                }
+            if(ch == ')' || ch == '}' || ch == ']'){
+                // check if the stack is empty
+                if(s.isEmpty()){
                     return false;
                 }
-                s.pop();
+                char top = s.pop();
+
+            if(ch == ')' && top != '(') return false;
+            if(ch == '}' && top != '{') return false;
+            if(ch == ']' && top != '[') return false;
+
+
+
             }
             
         }
 
+
+
+
+        
+
+
+
+
         return s.isEmpty();
     }
 
+
+
+
     public static void main(String[] args) {
-        String s = "4x((2-1()";
-        System.out.println(bracketMatching(s));
+        
     }
-    
 }
