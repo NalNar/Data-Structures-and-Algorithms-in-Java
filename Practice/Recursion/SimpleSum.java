229b8e8080067f8b064873c86cafb3078c60e088
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2026-03-11 06:54:03 +0000

    update

--- Practice/Recursion/SimpleSum.java
+++ Practice/Recursion/SimpleSum.java
@@ -9,11 +9,23 @@
 public class SimpleSum {
     public static int sumOfDigits(long n){
 
-        // base case when the digits is 1
-        if(n < 10){
-            return int n;
-
+        // base case when the digits is <0
+        if (n ==0){
+            return 0;
         }
+
+        // // if the n < 10 it already is one digit
+        // if(n < 10){
+        //     return (int) n;
+        // }
+        
+        // pick the last digit and sum again
+                
+        return (int) n%10 + sumOfDigits(n/10);
+    }
+    public static void main(String[] args) {
+        int a = sumOfDigits(390);
+        System.out.println(a);
     }
     
 }
